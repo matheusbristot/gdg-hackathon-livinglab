13ee61d48cd574dd1d4ce8a747d646e7e8eaa708
Matheus Bristot <matheus@jera.com.br>
2018-09-07 19:58:47 -0400

    Added crashlytics

--- presentation/src/main/java/com/hackaton/notice/FuckYouApplication.kt
+++ presentation/src/main/java/com/hackaton/notice/FuckYouApplication.kt
@@ -1,20 +1,32 @@
 package com.hackaton.notice
 
+import android.content.Context
 import android.support.multidex.MultiDexApplication
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.core.CrashlyticsCore
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.hackaton.notice.di.fuckYouAppModules
+import io.fabric.sdk.android.Fabric
 import org.koin.android.ext.android.startKoin
 
-class FuckYouApplication: MultiDexApplication() {
+
+class FuckYouApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this, fuckYouAppModules)
         startAnalytics()
+        initFabric(this)
     }
 
     private fun startAnalytics() {
-        if(!BuildConfig.DEBUG) {
+        if (!BuildConfig.DEBUG) {
             FirebaseAnalytics.getInstance(this)
         }
+    }
+
+    private fun initFabric(context: Context) {
+        val core = CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
+        val kit = Crashlytics.Builder().core(core).build()
+        Fabric.with(context, kit)
     }
 }
